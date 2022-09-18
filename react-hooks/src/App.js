75db5548a38145ccfe28299e3bf825d01d0eb10b
RaheelShaikh <44947713+RaheelShaikh@users.noreply.github.com>
2022-09-18 20:00:56 +0530

    Update App.js

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
   const [ marks, setMarks ] = useState("");
-  const [status, seStatus] = useState("");
+  const [result, setResult] = useState("");
 
   useEffect(() => {
     console.log("Marks:", marks);
@@ -12,11 +12,13 @@ function App() {
   }, [marks]);
 
   const getResult = (marks) => {
-    if (marks > 35) {
-      seStatus("Pass");
-    } else {
-      seStatus("Fail");
-    }
+    if (marks == "") {
+      setResult("");
+    } else if (marks > 35) {
+      setResult("Pass");
+    } else  if (marks < 35 ) {
+      setResult("Fail");
+    } 
 
   }
 
@@ -26,8 +28,8 @@ function App() {
       <h1>React Hooks Example</h1>
       <input type="text" value={marks} onChange= {(e) =>  setMarks(e.target.value)} placeholder="Enter Marks">
       </input>
-      <h2><p>{marks}</p></h2>
-      <h2><p>{status}</p></h2>
+      <h2><p>You Entered : {marks}</p></h2>
+      <h2><p>Your Result is : {result}</p></h2>
     </form>
   );
 }
